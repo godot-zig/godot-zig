2d19f647413e9d7fd7ef71129e1b2f994ff26206
Silver <silver@squirl.dev>
2024-04-30 07:36:35 +0100

    Clean up build.zig

--- build.zig
+++ build.zig
@@ -2,26 +2,16 @@ const std = @import("std");
 
 const api_path = "src/api";
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
-    const precision = b.option([]const u8, "precision", "double") orelse "float";
-    const arch = b.option([]const u8, "arch", "32") orelse "64";
-    const godot_path = b.option([]const u8, "godot", "godot path") orelse "godot";
 
-    const bind_step = createBindStep(b, target, precision, arch, godot_path);
+    const precision = b.option([]const u8, "precision", "Floating point precision, either `float` or `double` [default: `float`]") orelse "float";
+    const godot_path = b.option([]const u8, "godot", "Path to Godot engine binary [default: `godot`]") orelse "godot";
 
+    const bind_step = createBindStep(b, target, precision, godot_path);
+
+    // Example project
     const lib = b.addSharedLibrary(.{
         .name = "example",
         .root_source_file = .{ .path = "src/ExamplePlugin.zig" },
@@ -31,10 +21,9 @@ pub fn build(b: *std.Build) void {
 
     const build_options = b.addOptions();
     build_options.addOption([]const u8, "precision", precision);
-    build_options.addOption([]const u8, "arch", arch);
     lib.root_module.addOptions("build_options", build_options);
 
-    lib.addIncludePath(.{ .path = b.pathJoin(&.{ thisDir(), api_path }) });
+    lib.addIncludePath(b.path(api_path));
     lib.linkLibC();
     lib.step.dependOn(bind_step);
 
@@ -45,11 +34,11 @@ pub fn build(b: *std.Build) void {
         godot_path, "--path", "./project",
     });
     run_cmd.step.dependOn(b.getInstallStep());
-    const run_step = b.step("run", "run with Godot");
+    const run_step = b.step("run", "Run with Godot");
     run_step.dependOn(&run_cmd.step);
 }
 
-pub fn createBindStep(b: *std.Build, target: std.Build.ResolvedTarget, precision: []const u8, arch: []const u8, godot_path: []const u8) *std.Build.Step {
+pub fn createBindStep(b: *std.Build, target: std.Build.ResolvedTarget, precision: []const u8, godot_path: []const u8) *std.Build.Step {
     const dump_cmd = b.addSystemCommand(&.{
         godot_path, "--dump-extension-api", "--dump-gdextension-interface", "--headless",
     });
@@ -64,7 +53,11 @@ pub fn createBindStep(b: *std.Build, target: std.Build.ResolvedTarget, precision
 
     const generate_binding = std.Build.Step.Run.create(b, "bind_godot");
     generate_binding.addArtifactArg(binding_generator);
-    generate_binding.addArgs(&.{ out_path, precision, arch });
+    generate_binding.addArgs(&.{
+        out_path,
+        precision,
+        b.fmt("{}", .{target.result.ptrBitWidth()}),
+    });
 
     const bind_step = b.step("bind", "generate godot bindings");
     bind_step.dependOn(&generate_binding.step);
